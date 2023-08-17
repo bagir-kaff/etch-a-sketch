6296a3580c5689f68258aa24c228725d2bdafbb4
bagir-kaff <trexhilang@gmail.com>
2023-08-17 22:49:47 +0330

    Add set size function

--- external.js
+++ external.js
@@ -1,28 +1,48 @@
 const body = document.querySelector('body')
-const container = document.createElement('div')
-let n = 16; 
+let container = document.createElement('div')
+const setSizeButton = document.querySelector('.size')
+let n = 16;
 
+function setNewGrid(){
+  do{
+    n = Number(prompt('input resolution max 100'))
+    if(container.childNodes[0]){
+      body.removeChild(container)
+    }
+    setGrid()
+  }while(!(n>=0 && n<=100))
+}
+setSizeButton.addEventListener('click',setNewGrid)
 container.classList.add('container')
-
 function changeBackground(e){
     if (this.classList.value === 'pixel'){
         this.style.backgroundColor = 'black';
     }
 };
-
-for (let i=1;i<=n;i++){   // n x
-  const div = document.createElement('div')
-  div.classList.add('flexdiv');
-  for(let i =1;i<=n;i++){    // n
-    const tempDiv = document.createElement('div');
-    // tempDiv.textContent = i;
-    tempDiv.classList.add('pixel')
-    div.appendChild(tempDiv);
+function formGrid(){
+  for (let i=1;i<=n;i++){   // n x
+    const div = document.createElement('div')
+    div.classList.add('flexdiv');
+    for(let i =1;i<=n;i++){    // n
+      const tempDiv = document.createElement('div');
+      // tempDiv.textContent = i;
+      tempDiv.classList.add('pixel')
+      div.appendChild(tempDiv);
+    }
+    container.appendChild(div)
   }
-  container.appendChild(div)
 }
-const divs = container.querySelectorAll('.pixel')
-divs.forEach(div => {
-    div.addEventListener('mouseover',changeBackground)
-})
-body.appendChild(container)
+
+function setGrid(){
+  if(container){
+    container = document.createElement('div')
+    container.classList.add('container')
+  }
+  formGrid()
+  const divs = container.querySelectorAll('.pixel')
+  divs.forEach(div => {
+      div.addEventListener('mouseover',changeBackground)
+  })
+  body.appendChild(container)
+}
+setGrid()
