4cf4f5bf6473c890ee410f4bf21a4b5c4e5c88e5
bagir-kaff <trexhilang@gmail.com>
2023-08-18 07:29:32 +0330

    Tidy the syntax and optimise it
    there are two major things i did, first, i added semicolon to the and
    *end of every line and tidy repeated line, instead of using forEach to
    add eventlistener to all pixel classes, i used already written loop,
    secondly, the min and max value for n*n grid, the clamp also included
    setGrid function,which means after accepting a number, instead of
    checking it first, the function computes it first, and then check the
    number, which is bad, really bad, i inputed 1222 to test if the clamps works, well, it crashed the browser, the music i had, was lagging,
    the cursor wouldnt move, the browser stopped, just stop,
    
    thats it

--- external.js
+++ external.js
@@ -1,48 +1,48 @@
-const body = document.querySelector('body')
-let container = document.createElement('div')
-const setSizeButton = document.querySelector('.size')
+const body = document.querySelector('body');
+let container = document.createElement('div');
+container.classList.add('container');
+//it was written like const container
+const setSizeButton = document.querySelector('.size');
+setSizeButton.addEventListener('click',setNewGrid);
 let n = 16;
 
 function setNewGrid(){
   do{
-    n = Number(prompt('input resolution max 100'))
-    if(container.childNodes[0]){
-      body.removeChild(container)
+    n = Number(prompt('input resolution max 100'));
+  }while(!(n>=0 && n<=100)); //was at the end of dis function
+    if(container.childNodes[0]){ // basicaly checks if container exist
+      body.removeChild(container);
     }
-    setGrid()
-  }while(!(n>=0 && n<=100))
+    setGrid();
 }
-setSizeButton.addEventListener('click',setNewGrid)
-container.classList.add('container')
 function changeBackground(e){
     if (this.classList.value === 'pixel'){
         this.style.backgroundColor = 'black';
     }
-};
+}
 function formGrid(){
-  for (let i=1;i<=n;i++){   // n x
-    const div = document.createElement('div')
+  for (let i=1;i<=n;i++){
+    const div = document.createElement('div');
     div.classList.add('flexdiv');
-    for(let i =1;i<=n;i++){    // n
+    for(let i =1;i<=n;i++){
       const tempDiv = document.createElement('div');
       // tempDiv.textContent = i;
-      tempDiv.classList.add('pixel')
+      tempDiv.classList.add('pixel');
+      //set height and width
       div.appendChild(tempDiv);
+      tempDiv.addEventListener('mouseover',changeBackground)
+      // it was written using forEach outside of this function
     }
-    container.appendChild(div)
+    container.appendChild(div);
+    //put a row of pixel into container
   }
 }
-
 function setGrid(){
   if(container){
-    container = document.createElement('div')
-    container.classList.add('container')
+    container = document.createElement('div'); //clean variable
+    container.classList.add('container');
   }
-  formGrid()
-  const divs = container.querySelectorAll('.pixel')
-  divs.forEach(div => {
-      div.addEventListener('mouseover',changeBackground)
-  })
-  body.appendChild(container)
+  formGrid();
+  body.appendChild(container);
 }
-setGrid()
+setGrid();
